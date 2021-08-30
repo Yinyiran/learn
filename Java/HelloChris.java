69d6f639864d445e5fa45834d52a5a5f75c81cca
yinyiran <yinyiran100@163.com>
2021-08-30 18:02:04 +0800

    java main

---
+++ Java/HelloChris.java
@@ -0,0 +1,17 @@
+public class HelloChris {
+  /**
+   * main
+   * 
+   * @param args any
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");// hello world
+    System.out.print("print");
+  }
+}
+
+class Person {
+  static void lll() {
+
+  }
+}
