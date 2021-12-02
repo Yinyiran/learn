7eb13f6b1d72c94ad31ff743602fe3e4946bc5bc
yinyiran <yinyiran100@163.com>
2021-12-02 17:33:39 +0800

    opp

---
+++ Java/TestJ.java
@@ -0,0 +1,11 @@
+
+
+public class TestJ {
+  public static void main(String[] args) {
+
+  }
+
+  public static void testFn() {
+    System.out.println("testFn");
+  }
+}
--- Java/extend.java
+++ Java/extend.java
@@ -1,13 +1,63 @@
+import java.util.Random;
+
 public class Extend {
   public static void main(String[] args) {
-
+    // geometricAreaTest();
+    animalTest();
   }
 
-  // static void areaTest() {
-  //   Geometric geome = new Geometric();
-  //   Circle c1 = new Circle();
-  //   geome.
-  // }
+  // Geometric
+  static void geometricAreaTest() {
+    Round r1 = new Round("black", 3, 5);
+    printGeometric(r1);
+    Rectangle r2 = new Rectangle("white", 2.0, 5.0);
+    printGeometric(r2);
+    Triangle t1 = new Triangle("white", 2.0, 5.0);
+    printGeometric(t1);
+    boolean isEual = equalGeomeArea(r1, r2);
+    System.out.println("面积是否相等：" + isEual);
+  }
+
+  public static void printGeometric(Geometric obj) {
+    System.out.println(getGeometricType(obj) + "，面积：" + obj.calArea());
+  }
+
+  public static boolean equalGeomeArea(Geometric o1, Geometric o2) {
+    return o1.calArea() == o2.calArea();
+  }
+
+  public static String getGeometricType(Geometric obj) {
+    if (obj instanceof Round) {
+      return "圆";
+    } else if (obj instanceof Rectangle) {
+      return "长方形";
+    } else if (obj instanceof Triangle) {
+      return "三角形";
+    } else {
+      return "基本类型";
+    }
+  }
+
+  // Animal
+  public static void animalTest() {
+    int key = new Random().nextInt(4);
+    System.out.println(key);
+    Animal animal = getAnimalInstance(key);
+    animal.eat();
+  }
+
+  public static Animal getAnimalInstance(int key) {
+    switch (key) {
+      case 0:
+        return new Cat();
+      case 1:
+        return new Dog();
+      case 3:
+        return new Sheep();
+      default:
+        return new Animal();
+    }
+  }
 }
 
 // 子类继承父类
@@ -16,7 +66,15 @@ public class Extend {
 
 class Geometric {
   protected String color;
-  protected double weight;
+  protected double with;
+
+  public Geometric() {
+  }
+
+  public Geometric(String color, double with) {
+    this.color = color;
+    this.with = with;
+  }
 
   public String getColor() {
     return color;
@@ -27,32 +85,23 @@ class Geometric {
   }
 
   public double getWeight() {
-    return weight;
+    return with;
   }
 
-  public void setWeight(double weight) {
-    this.weight = weight;
-  }
-
-  public Geometric(String color, double weight) {
-    this.color = color;
-    this.weight = weight;
-  }
-
-  public Geometric() {
+  public void setWeight(double with) {
+    this.with = with;
   }
 
   public double calArea() {
     return 0.0;
   }
-
 }
 
 class Round extends Geometric {
   private double radius;
 
-  public Round(String color, double weight, double radius) {
-    super(color, weight);
+  public Round(String color, double radius, double with) {
+    super(color, with);
     this.radius = radius;
   }
 
@@ -71,21 +120,11 @@ class Round extends Geometric {
 }
 
 class Rectangle extends Geometric {
-  private double width;
   private double height;
 
-  public Rectangle(double width, double height, String color, double weight) {
-    super(color, weight);
-    this.width = width;
+  public Rectangle(String color, double height, double with) {
+    super(color, with);
     this.height = height;
-  }
-
-  public double getWidth() {
-    return width;
-  }
-
-  public void setWidth(double width) {
-    this.width = width;
   }
 
   public double getHeight() {
@@ -98,6 +137,54 @@ class Rectangle extends Geometric {
 
   @Override
   public double calArea() {
-    return width * height;
+    return super.with * height;
+  }
+}
+
+class Triangle extends Geometric {
+
+  private double height;
+
+  public Triangle(String color, double height, double with) {
+    super(color, with);
+    this.height = height;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  @Override
+  public double calArea() {
+    return height * super.with / 2;
+  }
+}
+
+// 面试题： 多态是编译时行为还是运行时行为？如何证明？
+class Animal {
+  protected void eat() {
+    System.out.println("animal eat food");
+  }
+}
+
+class Cat extends Animal {
+  protected void eat() {
+    System.out.println("cat eat food");
+  }
+}
+
+class Dog extends Animal {
+  protected void eat() {
+    System.out.println("dog eat food");
+  }
+}
+
+class Sheep extends Animal {
+  protected void eat() {
+    System.out.println("sheep eat food");
   }
 }
