c6c36de0c4fb920e6aed74ff7066dbc376698090
yinyiran <yinyiran100@163.com>
2021-12-27 17:31:18 +0800

    thread

--- Java/CollectionT.java
+++ Java/CollectionT.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.Date;
 
 public class CollectionT {
   public static void main(String[] args) {
@@ -8,15 +8,15 @@ public class CollectionT {
   }
 
   public static void test() {
-    Collection coll = new ArrayList();
+    // Collection coll = new ArrayList();
 
-    // add(Object e):将元素e添加到集合coll中
-    coll.add("AA");
-    coll.add("BB");
-    coll.add(123); // 自动装箱
-    coll.add(new Date());
+    // // add(Object e):将元素e添加到集合coll中
+    // coll.add("AA");
+    // coll.add("BB");
+    // coll.add(123); // 自动装箱
+    // coll.add(new Date());
 
-    // size():获取添加的元素的个数
-    System.out.println(coll.size()); // 4
+    // // size():获取添加的元素的个数
+    // System.out.println(coll.size()); // 4
   }
 }
--- Java/MultiThread.java
+++ Java/MultiThread.java
@@ -1,4 +1,5 @@
 import java.lang.Runnable;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 基本概念：程序、进程、线程
@@ -30,7 +31,8 @@ public class MultiThread {
   public static void main(String[] args) {
     // runMyThread();
     // ticketTest();
-    ticketTest2();
+    // ticketTest2();
+    lockTicketTest();
   }
 
   public static void runMyThread() {
@@ -75,6 +77,18 @@ public class MultiThread {
     w3.start();
   }
 
+  public static void lockTicketTest() {
+    LockTicket t = new LockTicket();
+    Thread t1 = new Thread(t);
+    Thread t2 = new Thread(t);
+    Thread t3 = new Thread(t);
+    t1.setName("窗口1");
+    t2.setName("窗口2");
+    t3.setName("窗口3");
+    t1.start();
+    t2.start();
+    t3.start();
+  }
 }
 
 // 1. 创建一个继承于Thread类的子类
@@ -135,7 +149,7 @@ class Wicket implements Runnable {
     ticket--;
   }
 }
- 
+
 // 方法2
 class Wicket2 extends Thread {
   private static int ticket = 100;
@@ -159,5 +173,188 @@ class Wicket2 extends Thread {
     }
     System.out.println(Thread.currentThread().getName() + ": 卖票，票号为：" + ticket);
     ticket--;
+  }
+}
+
+// 线程死锁
+class ThA {
+  public synchronized void foo(ThB b) {
+    System.out.println("当前线程名称" + Thread.currentThread().getName() + "进入了A实例的foo方法");
+    try {
+      Thread.sleep(200);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println("当前线程名称" + Thread.currentThread().getName() + "企图调用B实例的last方法");
+    b.last();
+  }
+
+  public synchronized void last() {
+    System.out.println("进入了A类的last方法内部");
+  }
+}
+
+class ThB {
+  public synchronized void bar(ThA a) {
+    System.out.println("当前线程名称" + Thread.currentThread().getName() + "进入了B实例的bar方法");
+    try {
+      Thread.sleep(200);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println("当前线程名称" + Thread.currentThread().getName() + "企图调用A实例的last方法");
+    a.last();
+  }
+
+  public synchronized void last() {
+    System.out.println("进入了B类的last方法内部");
+  }
+
+}
+
+class DeadLock implements Runnable {
+  ThA a = new ThA();
+  ThB b = new ThB();
+
+  public void init() {
+    Thread.currentThread().setName("-主线程-");
+    a.foo(b);
+    System.out.println("进入了主线程之后");
+  }
+
+  public void run() {
+    Thread.currentThread().setName("副线程");
+    b.bar(a);
+    System.out.println("进入副线程之后");
+  }
+
+  public static void main(String[] args) {
+    DeadLock dl = new DeadLock();
+    new Thread(dl).start();
+    dl.init();
+  }
+}
+
+// 解决线程安全问题方式 三：Lock
+/**
+ * 1. 面试题：synchronized 与 Lock的异同？
+ * 相同：二者都可以解决线程安全问题
+ * 不同：synchronized机制在执行完相应的同步代码以后，自动的释放同步监视器
+ * Lock需要手动的启动同步（lock()），同时结束同步也需要手动的实现（unlock()）
+ * 2. 优先使用顺序：
+ * Lock 同步代码块（已经进入了方法体，分配了相应资源）同步方法（在方法体之外）
+ */
+class LockTicket implements Runnable {
+  private int ticket = 100;
+  private ReentrantLock lock = new ReentrantLock();
+
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        lock.lock();
+        if (ticket > 0) {
+          try {
+            Thread.sleep(100);
+
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+          System.out.println(Thread.currentThread().getName() + "售票，票号为：" + ticket);
+          ticket--;
+        } else {
+          break;
+        }
+      } finally {
+        lock.unlock();
+      }
+    }
+  }
+}
+
+/**
+ * 线程通信的例子：使用两个线程打印1-100。线程1, 线程2 交替打印
+ *
+ * 涉及到的三个方法：
+ * wait():一旦执行此方法，当前线程就进入阻塞状态，并释放同步监视器。
+ * notify():一旦执行此方法，就会唤醒被wait的一个线程。如果有多个线程被wait，就唤醒优先级高的那个。
+ * notifyAll():一旦执行此方法，就会唤醒所有被wait的线程。
+ *
+ * 说明：
+ * 1.wait()，notify()，notifyAll()三个方法必须使用在同步代码块或同步方法中。
+ * 2.wait()，notify()，notifyAll()三个方法的调用者必须是同步代码块或同步方法中的同步监视器。
+ * 否则，会出现IllegalMonitorStateException异常
+ * 3.wait()，notify()，notifyAll()三个方法是定义在java.lang.Object类中。
+ */
+
+/*
+ * 线程通信的应用：经典例题：生产者/消费者问题
+ *
+ * 生产者(Productor)将产品交给店员(Clerk)，而消费者(Customer)从店员处取走产品，
+ * 店员一次只能持有固定数量的产品(比如:20），如果生产者试图生产更多的产品，
+ * 店员会叫生产者停一下，如果店中有空位放产品了再通知生产者继续生产；
+ * 如果店中没有产品了，店员会告诉消费者等一下，
+ * 如果店中有产品了再通知消费者来取走产品。
+ *
+ * 分析：
+ * 1.是否是多线程的问题？是，生产者的线程，消费者的线程
+ * 2.是否有共享数据的问题？是，店员、产品、产品数
+ * 3.如何解决线程的安全问题？同步机制，有三种方法
+ * 4.是否涉及线程的通信？是
+ */
+
+class Clerk {
+  private int productCount = 0;
+
+  // 生产产品
+  public synchronized void produceProduct() {
+    if (productCount < 20) {
+      productCount++;
+      System.out.println(Thread.currentThread().getName() + ", 开始生产第" + productCount + "个产品");
+      notify();
+    } else {
+      try {
+        wait();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  // 消费产品
+  public synchronized void consumeProduct() {
+    if (productCount > 0) {
+      System.out.println(Thread.currentThread().getName() + ", 消费第" + productCount + "个产品");
+      productCount--;
+      notify();
+    } else {
+      try {
+        wait();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+// 生产
+class Producer extends Thread {
+  private Clerk clerk;
+
+  public Producer(Clerk clerk) {
+    this.clerk = clerk;
+  }
+
+  @Override
+  public void run() {
+    System.out.println(getName() + ", 开始生产产品 ......");
+    while (true) {
+      try {
+        Thread.sleep(10);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      clerk.produceProduct();
+    }
   }
 }
